65c825f09ee8e15965e71200b467e509c5555af6
Jayers01 <184981870+JoshAyersSBT@users.noreply.github.com>
2025-07-22 19:58:38 -0400

    Update setupAP.py

--- setupAP.py
+++ setupAP.py
@@ -262,7 +262,7 @@ $HTTP["host"] =~ ".*" {
 def configure_apache_for_wordpress():
     log_info("Configuring Apache to serve WordPress...")
 
-    wp_path = "/AP_mode_wordpress_launcher/www/"
+    wp_path = "/AP_mode_wordpress_launcher/www/captive-portal"
     apache_conf_path = "/etc/apache2/sites-available/pipress.conf"
 
     # Ensure WordPress directory exists
