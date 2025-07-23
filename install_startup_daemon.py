b4f38269a64fee3ae2bbb3117788c0902f236717
Jayers01 <184981870+JoshAyersSBT@users.noreply.github.com>
2025-07-23 17:07:16 -0400

    Update install_startup_daemon.py

--- install_startup_daemon.py
+++ install_startup_daemon.py
@@ -31,7 +31,7 @@ Wants=multi-user.target
 
 [Service]
 Type=simple
-ExecStart=/bin/bash /home/pi/AP_mode_wordpress_launcher/launch_wrapper.sh
+ExecStart=/bin/bash /AP_mode_wordpress_launcher/launch_wrapper.sh
 RemainAfterExit=true
 StandardOutput=journal
 StandardError=journal
