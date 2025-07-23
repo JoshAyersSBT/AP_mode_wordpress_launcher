56fde3aeeb5f72814f99aa15ff20f97a955c4c34
Jayers01 <184981870+JoshAyersSBT@users.noreply.github.com>
2025-07-23 18:08:19 -0400

    Update install_startup_daemon.py

--- install_startup_daemon.py
+++ install_startup_daemon.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 SERVICE_NAME = "lms.service"
 SERVICE_PATH = f"/etc/systemd/system/{SERVICE_NAME}"
-LMS_BINARY = "/AP_mode_wordpress_launcher/launch.sh"
+LMS_BINARY = "/AP_mode_wordpress_launcher/launch_wrapper.sh"
 
 def status(msg, level="INFO"):
     color = {
