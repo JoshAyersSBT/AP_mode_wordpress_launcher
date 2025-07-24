8de8bdf1349d574d986d49abce5e990d9fc5012c
Jayers01 <184981870+JoshAyersSBT@users.noreply.github.com>
2025-07-23 23:22:29 -0400

    Update install_startup_daemon.py

--- install_startup_daemon.py
+++ install_startup_daemon.py
@@ -25,14 +25,16 @@ After=graphical.target network-online.target hostapd.service dnsmasq.service apa
 Wants=graphical.target
 
 [Service]
-ExecStart=/usr/bin/python3 /AP_mode_wordpress_launcher/lms_gui_entry.py
+ExecStart=/usr/bin/python3 /home/pi/AP_mode_wordpress_launcher/lms_gui_entry.py
 User=root
 Group=root
-Restart=on-failure
+Restart=always
 RestartSec=5
+SuccessExitStatus=0 1
 
 [Install]
 WantedBy=default.target
+
 """
     with open(SERVICE_PATH, "w") as f:
         f.write(service_contents)
