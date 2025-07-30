34dedaea354a4d28d69e78f66e4d976f4005e944
Jayers01 <184981870+JoshAyersSBT@users.noreply.github.com>
2025-07-30 13:56:39 -0400

    Update lms_launcher.py

--- lms_launcher.py
+++ lms_launcher.py
@@ -66,8 +66,7 @@ class LMSLauncherGUI:
 
         self.log_box = tk.Text(root, height=6, width=50, font=("Courier", 10))
         self.log_box.pack(pady=5)
-        self.log_box.insert("end", "Starting required services...
-")
+        self.log_box.insert("end", "Starting required services...")
         self.log_box.configure(state="disabled")
 
         self.network_setup_done = False
